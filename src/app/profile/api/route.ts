b6d8e37fbb834136184d4fed8f3818b96def7cee
Ashfaaq-cmd <ashfaaqdulloo27@gmail.com>
2026-09-09 16:03:22 +0400

    Cookies

--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,15 +1,23 @@
 import { request } from "http";
 import { NextRequest } from "next/server";
-import { headers } from "next/headers";
+import { headers, cookies } from "next/headers";
 export async function GET(req: NextRequest) {
   // const requestHeaders = new Headers(req.headers);
   // console.log(requestHeaders.get("Authorization"));
 
   const headersList = await headers();
   console.log(headersList.get("Authorization"));
-   return new Response("<h1>Profile API Data<h1>", {
+
+  const theme = req.cookies.get("theme");
+  console.log(theme);
+  const cookieStore = await cookies();
+  cookieStore.set("results per page", "10");
+  console.log(cookieStore.get("results per page"));
+   return new Response
+   ("<h1>Profile API Data</h1>", {
   headers: {
     "Content-Type": "text/html",
+    "set-cookie" : "theme=dark",
   },
 }
 );
