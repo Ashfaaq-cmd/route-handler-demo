53a73f72df6f8d90fa5a80cf19ead1618a0cdd52
Ashfaaq-cmd <ashfaaqdulloo27@gmail.com>
2026-09-09 15:16:11 +0400

    Handling delete Request

--- src/app/comments/[id]/route.ts
+++ src/app/comments/[id]/route.ts
@@ -17,3 +17,11 @@ export async function PATH(request: Request, { params }: { params: Promise <{ id
         }
     }
 }
+
+export async function DELETE(request: Request, { params }: { params: Promise <{ id: string }> }) {
+    const { id } = await params;
+    const index = comments.findIndex((comment) => comment.id === parseInt(id));
+    const deletedComment = comments.splice(index, 1);
+    return Response.json(deletedComment)
+
+}
