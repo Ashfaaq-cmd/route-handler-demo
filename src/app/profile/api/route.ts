25ff476e179a375d85d7d94ec33f87f61603da9c
Ashfaaq-cmd <ashfaaqdulloo27@gmail.com>
2026-09-09 15:56:30 +0400

    Headers in route handlers

--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,3 +1,17 @@
-export async function GET() {
-  return new Response("Profile API Data");
+import { request } from "http";
+import { NextRequest } from "next/server";
+import { headers } from "next/headers";
+export async function GET(req: NextRequest) {
+  // const requestHeaders = new Headers(req.headers);
+  // console.log(requestHeaders.get("Authorization"));
+
+  const headersList = await headers();
+  console.log(headersList.get("Authorization"));
+   return new Response("<h1>Profile API Data<h1>", {
+  headers: {
+    "Content-Type": "text/html",
+  },
+}
+);
+ 
 }
