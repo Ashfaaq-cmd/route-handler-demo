c19b97e18545c342ba2e2ff0c888344cc8c59bad
Ashfaaq-cmd <ashfaaqdulloo27@gmail.com>
2026-09-09 15:13:07 +0400

    Handling Patch Request

--- src/app/comments/[id]/route.ts
+++ src/app/comments/[id]/route.ts
@@ -4,3 +4,16 @@ const { id } = await params;
 const comment = comments.find((comment) => comment.id === parseInt(id));
 return Response.json(comment);
 }
+export async function PATH(request: Request, { params }: { params: Promise <{ id: string }> }) {
+    const { id } = await params;
+    const body = await request.json();
+    const { text } = body;
+    const index = comments.findIndex((comment) => comment.id === parseInt(id));
+    comments[index].text = text;
+    return Response.json(comments[index]),{
+        headers: {
+            'Content-Type': 'application/json',
+            status:200,
+        }
+    }
+}
